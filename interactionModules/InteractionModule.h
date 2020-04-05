3c7051e42202cdc4cffc89c504f52f208d4d3c20
fabio <fabio@fabio>
2020-04-05 15:15:32 -0300

    Criação da classe virtual InteractionModule; Adição do json.h

---
+++ interactionModules/InteractionModule.h
@@ -0,0 +1,21 @@
+
+
+class InteractionModule
+{
+
+public:
+	virtual void startModule();
+	virtual void setUserKeyList(list <user, key>);
+	virtual void stop Module(); 
+};
+
+
+
+class Communication
+{
+
+public:
+	virtual void startModule();
+	virtual void setUserKeyList(list <user, key>);
+	virtual void stop Module(); 
+};
