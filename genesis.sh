53d94c5cf7f1b7b506d31c3b9b0a2572a4e5df2c
Eyre Brasil <eyre@montevecchi.com.br>
2022-09-07 11:38:22 +0100

    #17 - Incluindo arquivo genesis
    Incluindo arquivo genesis que prepara o ambiente e instala o Ginga.

---
+++ genesis.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+echo "atualizando pacotes..."
+sudo apt-get update
+sudo apt-get upgrade
+
+echo "instalando pacotes dev essenciais..."
+sudo apt-get install build-essential libreadline-dev
+
+echo "instalando lua 5.2..."
+cd ~/Downloads
+curl -R -O "https://www.lua.org/ftp/lua-5.2.4.tar.gz"
+tar -zxf lua-5.2.4.tar.gz 
+cd lua-5.2.4/
+make linux test
+sudo make install
+cd ~
+
+echo "instalando git..."
+sudo apt-get install git
+
+
+
+echo "Instalando pip..."
+sudo apt-get install python3-pip
+
+echo "Instalando Meson..."
+pip3 install meson
+
+echo "Instalando Ninja..."
+apt-get install ninja-build
+
+echo "Instalando módulo JsonCpp..."
+cd ~/Downloads
+wget -c "https://github.com/open-source-parsers/jsoncpp/archive/master.zip"
+unzip master.zip
+mv jsoncpp-master jsoncpp
+cd jsoncpp
+mkdir dependencias
+cd dependencias
+~/.local/bin/meson ~/Downloads/jsoncpp .
+ninja
+ninja test
+cd ~
+echo "AVISO: Copiar a pasta jsoncpp em Downloads para a pasta de bibliotecas do seu projeto"
+
+
+echo "Instalando Ginga..." 
+cd ~/Desktop
+git clone https://github.com/TeleMidia/ginga.git
+git clone https://github.com/TeleMidia/nclua.git
+sudo apt-get install -y git gcc g++ autotools-dev dh-autoreconf \
+  liblua5.2-dev libglib2.0-dev libpango1.0-dev \
+  librsvg2-dev libgtk-3-dev libsoup2.4-dev -qq
+cd nclua
+./bootstrap
+./configure --prefix=/usr/
+make
+sudo make install
+cd ..
+sudo apt-get install -y git gcc g++ autotools-dev dh-autoreconf \
+    cmake cmake-data liblua5.2-dev libglib2.0-dev libpango1.0-dev \
+    librsvg2-dev libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev \
+    gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-libav \
+    libgtk-3-dev libsoup2.4-dev -qq
+cd ginga
+./bootstrap
+./configure --prefix=/usr/
+make
+cd ~
+
+
+echo "Instalando servidor Mosquitto..."
+sudo apt-get install mosquitto
+sudo apt-get install mosquitto-clients
+
+echo "Diretorio com Read.me do mosquitto: /usr/share/doc/mosquitto"
+echo "Diretorio com .config do mosquitto: /etc/mosquitto/mosquitto.conf"
+
+# Para teste sera necessario dois terminais de comando. Em um faca:
+# mosquitto_sub -t "test"
+# No outro faca:
+# mosquitto_pub -m "Mensagem" -t "test"
