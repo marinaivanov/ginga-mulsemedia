fd25a8436db3816c129fc76aa2335db72570e2ea
Eyre Brasil <eyre@montevecchi.com.br>
2022-09-07 13:07:09 +0100

    #18 - Limpando e incluindo comandos essenciais
    Retirando comandos desnecessários, correção dos caminhos de pestas e inclusão de instalações necessárias.

--- genesis.sh
+++ genesis.sh
@@ -7,47 +7,14 @@ sudo apt-get upgrade
 echo "instalando pacotes dev essenciais..."
 sudo apt-get install build-essential libreadline-dev
 
-echo "instalando lua 5.2..."
-cd ~/Downloads
-curl -R -O "https://www.lua.org/ftp/lua-5.2.4.tar.gz"
-tar -zxf lua-5.2.4.tar.gz 
-cd lua-5.2.4/
-make linux test
-sudo make install
-cd ~
+echo "instalando cURL..."
+sudo apt-get install curl
 
 echo "instalando git..."
 sudo apt-get install git
 
-
-
-echo "Instalando pip..."
-sudo apt-get install python3-pip
-
-echo "Instalando Meson..."
-pip3 install meson
-
-echo "Instalando Ninja..."
-apt-get install ninja-build
-
-echo "Instalando módulo JsonCpp..."
-cd ~/Downloads
-wget -c "https://github.com/open-source-parsers/jsoncpp/archive/master.zip"
-unzip master.zip
-mv jsoncpp-master jsoncpp
-cd jsoncpp
-mkdir dependencias
-cd dependencias
-~/.local/bin/meson ~/Downloads/jsoncpp .
-ninja
-ninja test
-cd ~
-echo "AVISO: Copiar a pasta jsoncpp em Downloads para a pasta de bibliotecas do seu projeto"
-
-
 echo "Instalando Ginga..." 
-cd ~/Desktop
-git clone https://github.com/TeleMidia/ginga.git
+cd ..
 git clone https://github.com/TeleMidia/nclua.git
 sudo apt-get install -y git gcc g++ autotools-dev dh-autoreconf \
   liblua5.2-dev libglib2.0-dev libpango1.0-dev \
@@ -63,10 +30,19 @@ sudo apt-get install -y git gcc g++ autotools-dev dh-autoreconf \
     librsvg2-dev libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev \
     gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-libav \
     libgtk-3-dev libsoup2.4-dev -qq
-cd ginga
+cd ginga-mulsemedia
 ./bootstrap
 ./configure --prefix=/usr/
 make
+cd ~
+
+echo "instalando lua 5.2..."
+cd ~/Downloads
+curl -R -O "https://www.lua.org/ftp/lua-5.2.4.tar.gz"
+tar -zxf lua-5.2.4.tar.gz 
+cd lua-5.2.4/
+make linux test
+sudo make install
 cd ~
 
 
