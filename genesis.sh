6b3c86f327e83fda8a5c392915bd0e2f51cdec6a
gingavm <eyre@montevecchi.com.br>
2022-09-14 03:40:32 -0700

    #28 incluindo prints em ingles e incluindo pkg necessarios

--- genesis.sh
+++ genesis.sh
@@ -2,34 +2,54 @@
 
 cd ..
 
-echo "atualizando pacotes..."
+echo "updating packages..."
 sudo apt-get update
 sudo apt-get upgrade
+echo
 
-echo "instalando pacotes dev essenciais..."
+echo "installing essencials dev packages..."
 sudo apt-get install build-essential libreadline-dev
+echo
 
-echo "instalando cURL..."
+echo "installing cURL..."
 sudo apt-get install curl
+echo
 
-echo "instalando git..."
+echo "installing git..."
 sudo apt-get install git
+echo
 
-echo "Instalando servidor e cliente Mosquitto..."
+echo "removing lo longer required packages..."
+sudo apt autoremove
+echo
+
+echo "installing pkg-config..."
+sudo apt-get install pkg-config
+echo
+
+echo "installing autoconf..."
+sudo apt-get install autoconf
+echo
+
+echo "installing Mosquitto's server and client..."
 sudo apt-get install mosquitto
 sudo apt-get install mosquitto-clients
-echo "Diretorio com Read.me do mosquitto: /usr/share/doc/mosquitto"
-echo "Diretorio com .config do mosquitto: /etc/mosquitto/mosquitto.conf"
+echo "Mosquitto's file Read.me is in /usr/share/doc/mosquitto"
+echo "Mosquitto's file .config is in /etc/mosquitto/mosquitto.conf"
+echo
 
-echo "Instalando biblioteca so Tobii e Gerenciador Tobii Pro.."
+echo "installing Tobii library and the manager Tobii Pro.."
 git clone https://github.com/Eitol/tobii_eye_tracker_linux_installer.git
 cd tobii_eye_tracker_linux_installer
 sudo bash ./install_all.sh
 cd ..
 sudo rm -r tobii_eye_tracker_linux_installer
+echo
+echo "correcting dependencies..."
+sudo apt --fix-broken install
+echo
 
-
-echo "Instalando NCLua..." 
+echo "installing NCLua..." 
 git clone https://github.com/TeleMidia/nclua.git
 sudo apt-get install -y git gcc g++ autotools-dev dh-autoreconf \
   liblua5.2-dev libglib2.0-dev libpango1.0-dev \
@@ -39,19 +59,21 @@ cd nclua
 ./configure --prefix=/usr/
 make
 sudo make install
+echo
 
 cd ..
 
-echo "Instalando Restclient para comunicacao com Moodo..."
+echo "installing Restclient for communication with Moodo..."
 git clone https://github.com/mrtazz/restclient-cpp.git
 cd restclient-cpp
 ./autogen.sh
 ./configure
 sudo make install
+echo
 
 cd ..
 
-echo "Instalando Ginga..." 
+echo "installing Ginga..." 
 sudo apt-get install -y git gcc g++ autotools-dev dh-autoreconf \
     cmake cmake-data liblua5.2-dev libglib2.0-dev libpango1.0-dev \
     librsvg2-dev libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev \
@@ -61,10 +83,14 @@ cd ginga-mulsemedia
 ./bootstrap
 ./configure --prefix=/usr/
 make
+echo
 
-echo "Criando diretorios de arquivos necessários para o Ginga..."
+echo "Creating file directories needed to running the Ginga..."
 mkdir ~/gingaFiles
 mkdir ~/gingaFiles/users
 mkdir ~/gingaFiles/interactionConfig
 cp interaction-config.json ~/gingaFiles/interactionConfig
+echo
+
+cd ..
 
